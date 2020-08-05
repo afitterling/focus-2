3c0c5fe66a68a4f9d4f7b5e3e997f59e23618413
Alexander Fitterling <info@sp33c.de>
2020-08-05 10:22:32 +0800

    focus

--- src/focus/main.js
+++ src/focus/main.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { List, Segment } from 'semantic-ui-react'
 
-export const MainList = ({items, onDelete}) => (
-    <Segment inverted>
-    <List divided inverted relaxed>
-        {items.map(({id, title, desc}) => (
-            <List.Item key={id}>
-            <List.Content>
-                <List.Header onClick={onDelete(id)}><i className="icon close white"></i> {title}</List.Header>{desc}
-            </List.Content>
-            </List.Item>
-            )
-        )}
-    </List>
-</Segment>
-)
+export const MainList = ({items, onDelete}) => {
+    //const stopEvent = (e) => {e.stopPropagation();}
+    return (
+        <Segment inverted>
+        <List divided inverted relaxed>
+            {items.map(({id, title, desc}) => (
+                <div class="item" key={id}>
+                    <div class="content">
+                        <div class="header"><button className="button ui" onClick={onDelete(id)}><i className="icon close"></i></button>{title}</div>
+                        {desc}                        
+                    </div>
+                </div>                
+                )
+            )}
+        </List>
+    </Segment>
+    );
+}
