df5c00af720e8ee562db6e203a6673e0efb86f0f
Alexander Fitterling <info@sp33c.de>
2020-08-08 08:50:58 +0800

    console

--- src/App.js
+++ src/App.js
@@ -2,15 +2,7 @@ import React from 'react';
 import './App.css';
 import { v4 as uuidv4 } from 'uuid';
 import { Items } from './pages/items';
-/* const dimensions = [
-  {key: 'career', label: 'Job Career'},
-  {key: 'reconsilation', label: 'Life/Freedom'},
-  {key: 'leisure', label: 'Leisure'},
-  {key: 'health', label: 'Health'},
-  {key: 'family', label: 'Family'},
-  {key: 'sustain', label: 'Sustainability'}
-  ];
- */
+
 class App extends React.Component {
 
   constructor () {
@@ -24,7 +16,6 @@ class App extends React.Component {
 
   onAdd = (formParams) => {
     const newItem = {id: uuidv4(), ...formParams};
-    console.log(newItem);
     this.setState({items: [...this.state.items, newItem]});    
     localStorage.setItem('items', JSON.stringify([...this.state.items, newItem]));
     this.setState({showNewForm: false});
@@ -49,7 +40,6 @@ class App extends React.Component {
   }
 
   onDeleteItem = (id) => () => {
-    console.log('onDelete', id);
     const items = this.state.items.filter( i => i.id !== id);
     this.setState({items: items});
     localStorage.setItem('items', JSON.stringify([...items]));
@@ -57,30 +47,8 @@ class App extends React.Component {
 
   componentDidMount(){
     const items = JSON.parse(localStorage.getItem('items')) || [];
-    console.log(items);
-    const values = items.map(
-      (i) => {
-        return {
-          key: i.id,
-          label: i.title,
-          values: {
-            leisure: parseInt(i.leisure) || 0,
-            career: parseInt(i.career) || 0,
-            reconsilation: parseInt(i.reconsilation) || 0,
-            health: parseInt(i.health)  || 0,
-            family: parseInt(i.family)  || 0,
-            sustain: parseInt(i.sustain)  || 0
-          }
-        }
-      }
-    )
-;
-    console.log(
-      values
-    );
     this.setState({
-      items: items,
-      values: values
+      items: items
     });    
   }
 
