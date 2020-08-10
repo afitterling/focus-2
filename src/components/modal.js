04b9bee9fd9ad2c3925e960187b4c62005ceb346
Alexander Fitterling <info@sp33c.de>
2020-08-10 12:53:56 +0800

    basic modal variant

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,38 +1,21 @@
 import React from 'react'
-import { Button, Header, Icon, Modal } from 'semantic-ui-react'
+import { Header, Modal } from 'semantic-ui-react'
 
-function ModalBasic() {
-  const [open, setOpen] = React.useState(false)
+function ModalBasic({title, children}) {
+  const [open] = React.useState(true)
 
   return (
     <Modal
-      basic
-      onClose={() => setOpen(false)}
-      onOpen={() => setOpen(true)}
       open={open}
-      size='small'
-      trigger={<Button>Basic Modal</Button>}
     >
-      <Header icon>
-        <Icon name='archive' />
-        Archive Old Messages
-      </Header>
-      <Modal.Content>
-        <p>
-          Your inbox is getting full, would you like us to enable automatic
-          archiving of old messages?
-        </p>
+      <Modal.Content image>        
+        <Modal.Description>
+          <Header>{title}</Header>
+          {children}
+        </Modal.Description>
       </Modal.Content>
-      <Modal.Actions>
-        <Button basic color='red' inverted onClick={() => setOpen(false)}>
-          <Icon name='remove' /> No
-        </Button>
-        <Button color='green' inverted onClick={() => setOpen(false)}>
-          <Icon name='checkmark' /> Yes
-        </Button>
-      </Modal.Actions>
     </Modal>
-  )
+    )
 }
 
 export default ModalBasic
--- src/pages/items.js
+++ src/pages/items.js
@@ -1,64 +1,71 @@
 import React from 'react';
-import {GenericTable} from '../components/generic-table/table';
-import {ItemForm} from '../components/generic-form/form';
+import { GenericTable } from '../components/generic-table/table';
+import { ItemForm } from '../components/generic-form/form';
+import ModalBasic from '../components/modal';
 
 export class Items extends React.Component {
-    
-    constructor(){
+
+    constructor() {
         super();
-        this.state = {showNewForm: false};
+        this.state = { showNewForm: false };
         this.onNewItem = this.onNewItem.bind(this);
         this.onCancel = this.onCancel.bind(this);
     }
 
     onItemAdd = (item) => {
         this.props.onItemAdd(item);
-        this.setState({showNewForm: false});
+        this.setState({ showNewForm: false });
     }
 
-    onNewItem () {
-        this.setState({showNewForm: true});
+    onNewItem() {
+        this.setState({ showNewForm: true });
     }
 
-    onCancel () {
-        this.setState({showNewForm: false, showEditForm: false, activeItem: null});
+    onCancel() {
+        this.setState({ showNewForm: false, showEditForm: false, activeItem: null });
     }
 
     onCellClick = (id) => () => {
         if (id && !this.state.showNewForm) {
-            this.setState({showEditForm: true, activeItem: this.props.items.find( i => i.id === id)});
+            this.setState({ showEditForm: true, activeItem: this.props.items.find(i => i.id === id) });
         }
     }
 
     onUpdate = (item) => {
         const ok = this.props.onUpdateItem(item);
-        this.setState({showEditForm: !ok, activeItem: null});
+        this.setState({ showEditForm: !ok, activeItem: null });
     }
 
-    render(){
+    render() {
         return (
             <div className="ui">
                 <GenericTable title={['Title', '']}
-                      displayName={['title', 'desc']}
-                      onCellClick={this.onCellClick}
-                      onDelete={this.props.onItemDelete}
-                      items={this.props.items}>
+                    displayName={['title', 'desc']}
+                    onCellClick={this.onCellClick}
+                    onDelete={this.props.onItemDelete}
+                    items={this.props.items}>
                 </GenericTable>
-                { 
-                    this.state.showNewForm ? <ItemForm onAddItem={this.onItemAdd} onSubmit={this.onItemAdd} onCancel={this.onCancel}></ItemForm> : null
+                {
+                    this.state.showNewForm ?
+                        <ModalBasic title='Add Item'>
+                            <ItemForm onAddItem={this.onItemAdd} onSubmit={this.onItemAdd} onCancel={this.onCancel}></ItemForm>
+                        </ModalBasic> : null
                 }
 
                 {
                     this.state.showEditForm ?
-                    <ItemForm item={this.state.activeItem} onUpdateItem={this.onUpdate} onCancel={this.onCancel}></ItemForm> :
-                    null
+                        <ModalBasic title='Edit Item'>
+                            <ItemForm item={this.state.activeItem} onUpdateItem={this.onUpdate} onCancel={this.onCancel}></ItemForm>
+                        </ModalBasic>
+                        :
+                        null
                 }
 
                 {
                     this.state.showNewForm || this.state.showEditForm ? null :
-                    <div className="ui" style={{padding: '5px'}}>
-                        <button className="ui circular button icon" onClick={this.onNewItem}><i className="circle icon plus"></i></button>
-                    </div>                
+                        <div className="ui" style={{ padding: '5px' }}>
+                            <button className="ui circular button icon" onClick={this.onNewItem}><i className="circle icon plus"></i></button>
+                        </div>
                 }
             </div>
         );
