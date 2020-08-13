49a72949ffee8bff49b88e61c98a88c9aa0c42ff
Alexander Fitterling <info@sp33c.de>
2020-08-14 02:40:07 +0800

    filter ok

--- src/pages/assistant.js
+++ src/pages/assistant.js
@@ -6,8 +6,8 @@ import { RadarChart as Radar } from '../components/graphs/radar';
 
 const congruent = (item, filter) => {    
     if (!item.dimensions) return false;
-    return !!dims.some(d => {
-        return filter[d.id] > parseInt(item.dimensions[d.id]);
+    return !!dims.every(d => {
+        return parseInt(item.dimensions[d.id]) >= filter[d.id];
     });
 }
 
