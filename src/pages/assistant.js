ac5ff85534dd57d2b895864e7762ffdd9bdae0e9
Alexander Fitterling <info@sp33c.de>
2020-08-14 02:18:16 +0800

    last change filter

--- src/pages/assistant.js
+++ src/pages/assistant.js
@@ -7,7 +7,7 @@ import { RadarChart as Radar } from '../components/graphs/radar';
 const congruent = (item, filter) => {    
     if (!item.dimensions) return false;
     return !!dims.every(d => {
-        return parseInt(item.dimensions[d.id]) <= (filter[d.id] + 1);
+        return filter[d.id] >= parseInt(item.dimensions[d.id]);
     });
 }
 
