f3e7ea71e0b607964fd866d94a8bb81d005ba4e5
Alexander Fitterling <info@sp33c.de>
2020-08-14 01:09:00 +0800

    new life dim

--- src/models/dimensions.js
+++ src/models/dimensions.js
@@ -3,6 +3,7 @@ export const Dimensions = [
     {id: 'leisure', name: 'Leisure'},
 //    {id: 'goal', name: 'Life Goal'}, --> different scheme
     {id: 'work', name: 'Work'},
+    {id: 'life', name: 'Life'},
     {id: 'balance', name: 'Life Balance'},
     {id: 'family', name: 'Family'},
     {id: 'health', name: 'Health'},
