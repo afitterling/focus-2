96da4807c7506edb58fb3f2ee600db5410b09c29
Alexander Fitterling <info@sp33c.de>
2020-08-18 11:45:20 +0800

    email

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -2,11 +2,29 @@ import React from 'react';
 import { Form } from 'semantic-ui-react';
 
 export class Settings extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {email: ''}
+    }
+
+    componentDidMount(){
+        this.setState({email: JSON.parse(localStorage.getItem('email'))});
+    }
+
+    onEmailChange = (e) => {
+        this.setState({email: e.target.value})
+        localStorage.setItem('email', JSON.stringify(e.target.value));
+    }
+
     render() {
         return (<Form>
             <Form.Field>
                 <label>UserId</label>
                 <input value={this.props.userId} onChange={()=>{}} disabled={false} />
+            </Form.Field>
+            <Form.Field>
+                <label>E-Mail</label>
+                <input value={this.state.email} onChange={this.onEmailChange} disabled={false} />
             </Form.Field>
         </Form>);
     }
