6f3a9e8b672117885123e4eec2ecfe338af72729
Alexander Fitterling <info@sp33c.de>
2020-08-20 00:34:30 +0800

    -finance

--- src/models/dimensions.js
+++ src/models/dimensions.js
@@ -22,7 +22,7 @@ const rawDims = [
 export const groups = [
     { id: 'importance', name: 'Importance', dims: ['urgent', 'important', 'emergency', 'leisure', 'life', 'goal'] },
     { id: 'main', name: 'Well-being', dims: ['alleviate', 'balance', 'leisure', 'life', 'social', 'family', 'health'] },
-    { id: 'type3', name: 'Fields of Activities', dims: ['work', 'sustain', 'jobsearch', 'life', 'finance' , 'family', 'learn'] }
+    { id: 'type3', name: 'Fields of Activities', dims: ['work', 'sustain', 'jobsearch', 'life', 'family', 'learn'] }
 ];
 
 const enhanceDimsWithGroups = (dims, groups) => { 
