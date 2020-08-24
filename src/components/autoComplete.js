6ef7d530b2d9a17d7a2bf15da959be14f582b2a9
Alexander Fitterling <info@sp33c.de>
2020-08-25 00:49:59 +0800

    -comment

--- src/components/autoComplete.js
+++ src/components/autoComplete.js
@@ -30,9 +30,6 @@ export class AutoComplete extends React.Component {
             );
     }
 
-    //    const _debouncedOnChange = (e) => {
-    //    };
-
     render = () => {
         return (
             (
