c515bd582907968ea7d11d036845cb20d3dd4b99
Alexander Fitterling <info@sp33c.de>
2020-08-24 17:16:30 +0800

    100% fix

--- src/components/autoComplete.js
+++ src/components/autoComplete.js
@@ -39,7 +39,7 @@ export class AutoComplete extends React.Component {
     render = () => {
         return (
             (
-                <div className="ui icon input container">
+                <div className="ui icon input" style={{width: '100%'}}>
                     <input type="text" value={this.state.form.searchString} onChange={this._onChange} placeholder="Search..." />
                     <i onClick={()=>{this.setState({ form:  {searchString: ''}});this.onDebounce('')}} className="circular trash link icon"></i>
                 </div>
